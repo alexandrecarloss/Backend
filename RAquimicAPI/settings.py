4178288feb139a63a068ddde84bff28f2be0875c
Carlos Alexandre <karurosualexandresouza123@gmail.com>
2025-12-29 20:08:08 -0400

    Atualizei o banco para supabase

--- RAquimicAPI/settings.py
+++ RAquimicAPI/settings.py
@@ -81,15 +81,28 @@ WSGI_APPLICATION = 'RAquimicAPI.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
 DATABASES = {
-    "default": dj_database_url.config(
-        default=os.environ.get("DATABASE_URL"),
-        conn_max_age=600,
-        ssl_require=True
-    )
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres.pawxittqpbqmsoasncro",
+        "PASSWORD": os.environ.get("DB_PASSWORD"),
+        "HOST": "aws-1-us-east-1.pooler.supabase.com",
+        "PORT": "6543",
+        "OPTIONS": {
+            "sslmode": "require",
+        },
+    }
 }
+
+
+# DATABASES = {
+#     "default": dj_database_url.config(
+#         default=os.environ.get("DATABASE_URL"),
+#         conn_max_age=600,
+#         ssl_require=True
+#     )
+# }
 
 
 # Password validation
